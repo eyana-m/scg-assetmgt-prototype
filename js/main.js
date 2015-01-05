a1ca5b39d8d39386496a811ba16f7c740d57d53d
kennedyespina <Kennedy@Kennedys-MacBook-Pro.local>
2015-01-05 21:48:34 +0800

    Added tabs. Still unfinished but tabs should work. Will work on the color scheme next

--- js/main.js
+++ js/main.js
@@ -1,10 +1,62 @@
 $(function () {
-    $('#barchart-1').highcharts({
+    $('#barchart-PBI-1').highcharts({
         chart: {
             type: 'column'
         },
         title: {
-            text: 'Current Inventory Count'
+            text: 'Philippine Batteries, Inc.'
+        },
+        xAxis: {
+            categories: ['Camera', 'Desktop', 'Laptop', 'Printer']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantity'
+            }
+        },
+        series: [{
+            name: 'Active',
+            data: [3, 5, 4, 1]
+        }, {
+            name: 'Inactive',
+            data: [2, 7, 5, 3]
+        }]
+    });
+});
+
+$(function () {
+    $('#barchart-STAM-1').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'STAM'
+        },
+        xAxis: {
+            categories: ['Camera', 'Desktop', 'Laptop', 'Printer']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantity'
+            }
+        },
+        series: [{
+            name: 'Active',
+            data: [3, 5, 4, 1]
+        }, {
+            name: 'Inactive',
+            data: [2, 7, 5, 3]
+        }]
+    });
+});
+
+$(function () {
+    $('#barchart-ALL-1').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'ALL'
         },
         xAxis: {
             categories: ['Camera', 'Desktop', 'Laptop', 'Printer']
