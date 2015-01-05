34f9229f2feb5649da8cd05ce17148af10736441
kennedyespina <Kennedy@Kennedys-MacBook-Pro.local>
2015-01-05 22:53:56 +0800

    Added stuff

--- js/main.js
+++ js/main.js
@@ -77,7 +77,71 @@ $(function () {
 });
 
 $(function () {
-    $('#barchart-2').highcharts({
+    $('#barchart-PBI-2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Inventory Status'
+        },
+        xAxis: {
+            categories: ['Desktop', 'Laptop', 'Monitor', 'Projector']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantity'
+            }
+        },
+        series: [{
+            name: 'Active',
+            data: [8, 17, 7, 0]
+        }, {
+            name: 'Service Unit',
+            data: [3, 11, 0, 0]
+        }, {
+            name: 'For Repair',
+            data: [0, 0, 0, 3]
+        }, {
+            name: 'Unknown',
+            data: [0, 0, 0, 0]
+        }],
+    });
+});
+
+$(function () {
+    $('#barchart-STAM-2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Inventory Status'
+        },
+        xAxis: {
+            categories: ['Desktop', 'Laptop', 'Monitor', 'Projector']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantity'
+            }
+        },
+        series: [{
+            name: 'Active',
+            data: [8, 17, 7, 0]
+        }, {
+            name: 'Service Unit',
+            data: [3, 11, 0, 0]
+        }, {
+            name: 'For Repair',
+            data: [0, 0, 0, 3]
+        }, {
+            name: 'Unknown',
+            data: [0, 0, 0, 0]
+        }],
+    });
+});
+
+$(function () {
+    $('#barchart-ALL-2').highcharts({
         chart: {
             type: 'column'
         },
